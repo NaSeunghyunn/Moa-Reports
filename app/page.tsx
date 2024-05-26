d4d713fab94c9ee901e821392129b5c18dde2c39
SeungHyunNa <nasbong077@gmail.com>
2024-05-26 14:44:43 +0900

    add Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>ホーム</div>;
+  return (
+    <div className="flex flex-col p-6 justify-between min-h-screen">
+      <div className="my-auto flex flex-col gap-2 items-center">
+        <h1 className="text-5xl font-semibold text-center text-gray-300">
+          MOA REPORTS.
+        </h1>
+      </div>
+      <div className="flex flex-col items-center gap-3">
+        <Link
+          href="/auth/create"
+          className="text-center w-full bg-green-500 rounded-md text-white py-2.5 hover:bg-green-400 transition-colors font-medium"
+        >
+          Join
+        </Link>
+        <div>
+          <span>すでにアカウントをお持ちですか？</span>
+          <Link href="/auth/login" className="hover:underline">
+            ログイン
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
