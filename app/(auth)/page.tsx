3f4b5c1942ff99ad5bcb441a2734ef0f6a55b397
SeungHyunNa <nasbong077@gmail.com>
2024-07-02 22:40:16 +0900

    정리1

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col p-6 justify-between h-screen">
+    <div className="flex flex-col p-6 justify-between min-h-screen h-full overflow-hidden">
       <div className="my-auto flex flex-col gap-2 items-center">
         <h1 className="text-5xl font-semibold text-center text-gray-300">
           MORE REPORTS.
